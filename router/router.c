a0f59accdfeb2d9d50630c6980422acd71834cb4
luciancahil <johndoe@example.com>
2025-06-21 11:59:50 -0700

    router running

--- router/router.c
+++ router/router.c
@@ -20,18 +20,29 @@ void addForwardEntry(uint32_t network, int netlength, int interface) {
  * Add an interface to your router. 
  */
 void addInterface(int interface) {
+    printf("Interface: %d\n", interface);
     // TODO: Implement this
 }
 
 void run() {
     int port = getDefaultPort();
     int fd = udp_open(port);
+    struct packet *pkt = malloc(12);
+    printf("%d\n", port);
+
+    int *interface = malloc(sizeof(int));
     // TODO: Implement this
+
+    while(1){
+        readpkt(fd, pkt, interface);
+    }
+    printf("Hello after\n");
 }
+
 
 int main(int argc, char **argv) {
     logConfig("router", "packet,error,failure");
-
+    printf("Yo!\n");
     char *configFileName = "router.config";
     if (argc > 1) {
 	configFileName = argv[1];
